a607889410421d0c8bfba354be73d85720c6d7d4
Younès Adem <younesadem7@gmail.com>
2025-01-08 21:31:55 +0100

    chore: temporarily remove cors whitelist

--- src/main/java/com/cotrip/auth/infrastructure/spring/CorsConfig.java
+++ src/main/java/com/cotrip/auth/infrastructure/spring/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.cotrip.auth.infrastructure.spring;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,15 +9,12 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-  @Value("${application.cors.origins}")
-  private String allowedOrigins;
-
   @Bean
   public CorsFilter corsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin(allowedOrigins);
+    config.addAllowedOriginPattern("http://*");
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
     source.registerCorsConfiguration("/**", config);
